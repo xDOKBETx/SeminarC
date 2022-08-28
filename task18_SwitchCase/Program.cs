f520179bde2dc75451f5ff9afcb08d17ce88fe76
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-28 19:26:46 +0300

    Add task18_SwitchCase

---
+++ task18_SwitchCase/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+switch (quarter)
+{
+    case 1:
+        {
+            Console.Write("x > 0 & y > 0");
+            break;
+        }
+
+    case 2:
+        {
+            Console.Write("x < 0 & y > 0");
+            break;
+        }
+
+    case 3:
+        {
+            Console.Write("x < 0 & y < 0");
+            break;
+        }
+
+    case 4:
+        {
+            Console.Write("x > 0 & y < 0");
+            break;
+        }
+
+    default:
+        {
+            Console.Write("Введенное число не соответсвует номеру четверти");
+            break;
+        }
+}
