263e9c157d941c506fecd364d95d81871f252dca
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-28 18:42:30 +0300

    Add task17

--- task17/Program.cs
+++ task17/Program.cs
@@ -7,7 +7,7 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if (x > 0 && y > 0)
+if (x > 0 && y > 0) // Проверка условий х и у
 {
     Console.WriteLine("1 четверть");
 }
